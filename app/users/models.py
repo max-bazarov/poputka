d3a2b39c5ddd1c9dc00ce9847507265e1d638afc
Max Bazarov <bazaroffm@gmail.com>
2023-04-29 21:56:47 +0300

    add initial user logic

---
+++ app/users/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String
+
+from database import Base
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    car = Column(ForeignKey('car.id'), nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
