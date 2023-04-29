acc5cf6584708801cab325c7cd7530b1a41a08ee
Max Bazarov <bazaroffm@gmail.com>
2023-04-29 22:03:06 +0300

    fix import

--- app/users/utils.py
+++ app/users/utils.py
@@ -3,7 +3,7 @@ from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.users.models import User
-from database import get_async_session
+from app.database import get_async_session
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
