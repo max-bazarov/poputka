085d1bb62367184e54f3f43ae1c276d9750570f9
Max Bazarov <bazaroffm@gmail.com>
2023-05-03 13:42:14 +0300

    refactor

--- app/core/service.py
+++ app/core/service.py
@@ -7,14 +7,7 @@ class BaseService:
     model = None
 
     @classmethod
-    async def get_by_id(cls, model_id):
-        async with async_session_maker() as session:
-            query = select(cls.model).filter_by(id=model_id)
-            result = await session.execute(query)
-            return result.scalar_one_or_none()
-
-    @classmethod
-    async def get_one_or_none(cls, **filter_by):
+    async def get_object_or_none(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
@@ -28,7 +21,7 @@ class BaseService:
             return result.scalars().all()
 
     @classmethod
-    async def add(cls, **data):
+    async def create(cls, **data):
         async with async_session_maker() as session:
             query = insert(cls.model).values(**data)
             await session.execute(query)
