53d7b06c65438b5e1518804a0a23106297fa98de
Max Bazarov <bazaroffm@gmail.com>
2023-05-10 21:36:27 +0300

    add create method

--- app/rides/service.py
+++ app/rides/service.py
@@ -1,6 +1,21 @@
+from sqlalchemy import insert
+
 from app.core.service import BaseService
+from app.database import async_session_maker
 from app.rides.models import Ride
+from app.users.models import User
 
 
 class RidesService(BaseService):
     model = Ride
+
+    @classmethod
+    async def create(cls, user: User, **data):
+        async with async_session_maker() as session:
+            query = (
+                insert(cls.model)
+                .values(driver=user.id, **data)
+                .returning(cls.model)
+            )
+            await session.execute(query)
+            await session.commit()
