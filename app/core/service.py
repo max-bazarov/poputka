9d3aaa98cce1d27dc3081d038091fec59285af0d
Max Bazarov <bazaroffm@gmail.com>
2023-05-10 21:32:11 +0300

    change scalars to mappings

--- app/core/service.py
+++ app/core/service.py
@@ -1,28 +1,29 @@
 from sqlalchemy import insert, select
 
-from app.database import async_session_maker
+from app.database import Base, async_session_maker
 
 
 class BaseService:
-    model = None
+    model: Base = None
 
     @classmethod
     async def get_object_or_none(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
-            return result.scalar_one_or_none()
+            return result.mappings().one_or_none()
 
     @classmethod
     async def get_all(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
-            return result.scalars().all()
+            return result.mappings().all()
 
     @classmethod
     async def create(cls, **data):
         async with async_session_maker() as session:
-            query = insert(cls.model).values(**data)
-            await session.execute(query)
+            query = insert(cls.model).values(**data).returning(cls.model)
+            result = await session.execute(query)
             await session.commit()
+            return result.mappings().first()
