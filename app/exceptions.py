025c30f27cfeb60aef8fe4b69f1146fa40466faf
Max Bazarov <bazaroffm@gmail.com>
2023-05-12 18:36:21 +0300

    add base exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,34 @@
+from typing import Any
+from fastapi import HTTPException, status
+
+
+class DetailedHTTPException(HTTPException):
+    STATUS_CODE = status.HTTP_500_INTERNAL_SERVER_ERROR
+    DETAIL = "Server error"
+
+    def __init__(self, **kwargs: dict[str, Any]) -> None:
+        super().__init__(
+            status_code=self.STATUS_CODE, detail=self.DETAIL, **kwargs
+        )
+
+
+class PermissionDeniedException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_403_FORBIDDEN
+    DETAIL = "Permission denied"
+
+
+class NotFoundException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+
+
+class BadRequestException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_400_BAD_REQUEST
+    DETAIL = "Bad Request"
+
+
+class NotAuthenticatedException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_401_UNAUTHORIZED
+    DETAIL = "User not authenticated"
+
+    def __init__(self) -> None:
+        super().__init__(headers={"WWW-Authenticate": "Bearer"})
