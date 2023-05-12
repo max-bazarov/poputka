e162e787b61421b54961a1f42e4fd6972b67306b
Max Bazarov <bazaroffm@gmail.com>
2023-05-12 18:37:14 +0300

    create user register router

---
+++ app/users/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, status
+from app.users.dependencies import valid_user_create
+
+from app.users.schemas import UserBaseReadSchema, UserCreateSchema
+from app.users.service import UserService
+
+
+router = APIRouter(prefix='/auth', tags=['Авторизация'])
+
+
+@router.post('/register', status_code=status.HTTP_201_CREATED)
+async def register_user(
+    auth_data: UserCreateSchema = Depends(valid_user_create),
+) -> UserBaseReadSchema:
+    user = await UserService.create(auth_data)
+    return {
+        'email': user['email'],
+        'username': user['username'],
+    }
