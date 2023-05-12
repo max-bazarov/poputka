11e8be0c8bc885ec6818a9f68eaaeb074dfc5bda
Max Bazarov <bazaroffm@gmail.com>
2023-05-12 18:33:59 +0300

    add users router

--- app/main.py
+++ app/main.py
@@ -1,22 +1,9 @@
 from fastapi import FastAPI
 
 from app.rides.router import router as rides_router
-from app.users.auth_config import auth_backend, fastapi_users
-from app.users.schemas import UserCreateSchema, UserReadSchema
+from app.users.router import router as users_router
 
 app = FastAPI(title='Попутка - поиск автомобильных попутчиков')
 
-
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth",
-    tags=["Auth"],
-)
-
-app.include_router(
-    fastapi_users.get_register_router(UserReadSchema, UserCreateSchema),
-    prefix="/auth",
-    tags=["Auth"],
-)
-
+app.include_router(users_router)
 app.include_router(rides_router)
