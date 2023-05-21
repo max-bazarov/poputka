361b5eeeacfd10d7623dabdadd6c7d26a17b4dcf
Max Bazarov <bazaroffm@gmail.com>
2023-05-21 15:14:39 +0300

    add empty login endpoint

--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Response, status
 from app.users.dependencies import valid_user_create
 
-from app.users.schemas import UserBaseReadSchema, UserCreateSchema
+from app.users.schemas import (
+    UserBaseReadSchema,
+    UserAuthSchema,
+    UserLoginAccessTokenResponseSchema,
+)
 from app.users.service import UserService
 
 
@@ -10,10 +14,17 @@ router = APIRouter(prefix='/auth', tags=['Авторизация'])
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
 async def register_user(
-    auth_data: UserCreateSchema = Depends(valid_user_create),
+    auth_data: UserAuthSchema = Depends(valid_user_create),
 ) -> UserBaseReadSchema:
     user = await UserService.create(auth_data)
     return {
         'email': user['email'],
         'username': user['username'],
     }
+
+
+@router.post('/login')
+async def login_user(
+    auth_data: UserAuthSchema, response: Response
+) -> UserLoginAccessTokenResponseSchema:
+    pass
