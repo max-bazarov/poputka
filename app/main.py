9fd279dfc10c73526d9d7591b4f14d851be3695b
Max Bazarov <bazaroffm@gmail.com>
2023-05-21 15:12:50 +0300

    add docs url configuration for nginx

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 from app.rides.router import router as rides_router
 from app.users.router import router as users_router
 
-app = FastAPI(title='Попутка - поиск автомобильных попутчиков')
+app = FastAPI(
+    title='Попутка - поиск автомобильных попутчиков', openapi_url='/api/docs'
+)
 
 app.include_router(users_router)
 app.include_router(rides_router)
