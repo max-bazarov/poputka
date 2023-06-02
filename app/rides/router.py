7086527739a23c1f030734fade37a8669d3e0362
samsegomof <wemagonof91@gmail.com>
2023-06-02 15:48:07 +0500

    First changes for rides enpoints

--- app/rides/router.py
+++ app/rides/router.py
@@ -8,11 +8,20 @@ from app.users.models import User
 router = APIRouter(prefix='/rides', tags=['Rides'])
 
 
-@router.get('')
+@router.get('/rides')
 async def get_rides() -> list[RideReadSchema]:
-    return await RidesService.get_all()
+    pass
+    #return await RidesService.get_all()
 
 
+@router.get('/rides/<ride_id:int>')
+async def get_ride_by_id() -> RideReadSchema:
+    pass
+
+
+@router.post('/rides/create')
+async def create_ride() -> RideCreateSchema:
+    pass
 # @router.post('')
 # async def add_ride(
 #     new_ride: RideCreateSchema, # user: User = Depends(current_user)
