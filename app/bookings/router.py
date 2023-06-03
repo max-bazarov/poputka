4f4ade9eeac90e158e088b77a6b41a3fe4c6becd
Andrej Troshin <darkusss13@ya.ru>
2023-06-03 10:50:21 +0300

    Fixed all bugs, made a router to delete the reservation.

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -9,18 +9,24 @@ router = APIRouter(
 
 
 # Получение списка своих бронирований.
-@router.get('/my_bookings/register', response_model=list[BookingReadSchema])
-async def my_bookings_register():
+@router.get('/bookings/{user_id}', response_model=list[BookingReadSchema])
+async def add_my_bookings():
     pass
 
 
 # Забронировать поездку.
 @router.post('/{ride_id}/bookings', response_model=list[BookingReadSchema])
-async def bookings_register(ride_id: int):
+async def add_bookings(ride_id: int):
     pass
 
 
 #  Узнать id-поездки.
 @router.get('/id', response_model=list[BookingReadSchema])
-async def bookings_id():
+async def add_bookings_id():
+    pass
+
+
+#  Отмена бронирования.
+@router.delete('/rides/{ride_id}/bookings/{booking_id}/', response_model=list[BookingReadSchema])
+async def delete_bookings_register():
     pass
