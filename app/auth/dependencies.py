926ea29a87d172f0b28cc29d82fb6722648b19d7
max-bazarov <74839068+max-bazarov@users.noreply.github.com>
2023-06-05 22:32:45 +0300

    #3 auth doc (#9)
    * add blank functions for tokens
    
    * add login, refresh token and logout endpoints
    
    * rename token schema
    
    * delete useless funtctionality for now
    
    * delete utils for now
    
    * add databases image
    
    * add refresh token model
    
    * move validators from utils

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -3,8 +3,18 @@ from app.auth.schemas import UserAuthRegisterSchema
 from app.auth.service import UserService
 
 
-async def valid_user_create(user: UserAuthRegisterSchema) -> UserAuthRegisterSchema:
+async def valid_user_create(
+    user: UserAuthRegisterSchema,
+) -> UserAuthRegisterSchema:
     if await UserService.get_object_or_none(email=user.email):
         raise EmailTakenException()
 
     return user
+
+
+async def valid_refresh_token():
+    pass
+
+
+async def valid_refresh_token_user():
+    pass
