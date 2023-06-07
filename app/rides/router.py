738fdcdabc70c7871d0eb924cc8d8a2f8dfe6ee7
samsegomof <wemagonof91@gmail.com>
2023-06-07 17:05:26 +0500

    Refactoring rides/router.py

--- app/rides/router.py
+++ app/rides/router.py
@@ -13,15 +13,15 @@ async def get_rides() -> list[RideReadSchema]:
 
 
 @router.get('/{ride_id}', status_code=status.HTTP_200_OK)
-async def get_ride_by_id() -> RideReadSchema:
+async def get_ride_by_id(ride_id) -> RideReadSchema:
     pass
 
 
 @router.post('/new_ride', status_code=status.HTTP_201_CREATED)
-async def create_ride() -> RideCreateSchema:
+async def create_ride(ride_id: int, new_ride: RideCreateSchema) -> RideReadSchema:
     pass
 
 
 @router.delete('/{ride_id}', status_code=status.HTTP_201_DELETED)
-async def delete_ride() -> RideReadSchema:
+async def delete_ride(ride_id: int) -> RideReadSchema:
     pass
