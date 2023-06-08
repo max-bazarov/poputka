cb4be3fb4ee2894442ad896fc46c0441c8b2e8f9
Max Bazarov <bazaroffm@gmail.com>
2023-06-08 10:56:12 +0300

    delete comments and fix post endpoint

--- app/rides/bookings/router.py
+++ app/rides/bookings/router.py
@@ -2,19 +2,18 @@ from app.rides.bookings.schemas import BookingReadSchema, BookingCreateSchema
 from app.rides.router import router
 
 
-# Получение списка своих бронирований.
 @router.get('/bookings')
 async def get_my_bookings() -> list[BookingReadSchema]:
     pass
 
 
-# Забронировать поездку.
-@router.post('/{ride_id}/booking')
-async def add_booking(ride_id: int, new_booking: BookingCreateSchema) -> BookingReadSchema:
+@router.post('/{ride_id}/bookings')
+async def add_booking(
+    ride_id: int, new_booking: BookingCreateSchema
+) -> BookingReadSchema:
     pass
 
 
-#  Отмена бронирования.
 @router.delete('/rides/{ride_id}/bookings/{booking_id}/')
 async def delete_booking(ride_id: int, booking_id: int) -> BookingReadSchema:
     pass
