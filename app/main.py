1bcc1da8f725c5439e36be9f9a18d9c3e04e56fc
Max Bazarov <bazaroffm@gmail.com>
2023-06-26 13:40:16 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.router import router as auth_router
 from app.rides.bookings.router import router as bookings_router
@@ -8,6 +9,20 @@ app = FastAPI(
     title='Попутка - поиск автомобильных попутчиков',
     root_path='/api'
 )
+
+origins = [
+    "http://frontend:5000",  # React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", 
+                   "Access-Control-Allow-Origin", "Authorization"],
+)
+
 
 app.include_router(auth_router)
 app.include_router(rides_router)
