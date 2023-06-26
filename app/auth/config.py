4286dbd189df495ce44430401ca5a1f86e596d4a
Max Bazarov <bazaroffm@gmail.com>
2023-06-26 13:36:59 +0300

    add new variables

--- app/auth/config.py
+++ app/auth/config.py
@@ -3,8 +3,16 @@ from pydantic import BaseSettings
 
 class AuthConfig(BaseSettings):
     REFRESH_TOKEN_EXP: int = 60 * 60 * 24 * 7  # 7 days
+    REFRESH_TOKEN_KEY: str
     ACCESS_TOKEN_EXP: int = 60 * 60  # 1 hour
     ACCESS_TOKEN_KEY: str
+
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: str
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
 
     JWT_ALG: str
     JWT_SECRET: str
