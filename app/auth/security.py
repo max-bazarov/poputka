43a379631150368f27f47c4000cde4b45ebb35c5
Max Bazarov <bazaroffm@gmail.com>
2023-06-26 13:39:04 +0300

    change password making logic

--- app/auth/security.py
+++ app/auth/security.py
@@ -1,12 +1,26 @@
-from passlib.context import CryptContext
-
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    return pwd_context.hash(password)
+    '''
+    The bcrypt hashing algorithm incorporates the salt value into
+    the resulting hashed password. When you use bcrypt.hashpw() to hash
+    a password, it combines the password bytes with the salt and performs
+    multiple rounds of hashing to generate the final hash.
+    The salt is stored together with the hashed password.
+    '''
+    encoded_password = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(encoded_password, salt)
 
 
-def check_password(password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(password, hashed_password)
+def check_password(password: str, password_from_db: str) -> bool:
+    '''
+    When you want to verify a password using bcrypt.checkpw(),
+    it extracts the salt from the stored hashed password and uses
+    it to hash the provided password.
+    Then, it compares the generated hash with the stored hash.
+    If the two hashes match, it means that the passwords are the same.
+    '''
+    password = password.encode('utf-8')
+    return bcrypt.checkpw(password, password_from_db)
