31e67b05845632abb70d2fcfbd1dd6bab0c576db
Max Bazarov <bazaroffm@gmail.com>
2023-06-27 15:36:16 +0300

    make send email synchronus

--- app/auth/email.py
+++ app/auth/email.py
@@ -11,21 +11,28 @@ from app.auth.jwt import create_access_token
 env = Environment(
     loader=FileSystemLoader(Path(__file__).parent.parent / 'templates')
 )
-template = env.get_template('email_verif.html')
 
 
-async def send_mail(user_email: EmailStr, user_name: str, user_id: int):
-    token = await create_access_token(user_id=user_id)
+def create_email(user_email: str, user_name: str, user_id: int):
+    token = create_access_token(user_id=user_id)
     body = {'name': user_name, 'token': token}
 
+    template = env.get_template('email_verif.html')
     rendered_content = template.render(**body)
 
     email = EmailMessage()
     email['Subject'] = 'Подтверждение почты'
     email['From'] = f'{config.MAIL_FROM_NAME} <{config.MAIL_FROM}>'
     email['To'] = user_email
-
     email.set_content(rendered_content, subtype='html')
+
+    return email
+
+
+def send_email(user_email: EmailStr, user_name: str, user_id: int):
+    email = create_email(
+        user_email=user_email, user_name=user_name, user_id=user_id
+    )
 
     with smtplib.SMTP_SSL(config.MAIL_SERVER, config.MAIL_PORT) as server:
         server.login(config.MAIL_USERNAME, config.MAIL_PASSWORD)
