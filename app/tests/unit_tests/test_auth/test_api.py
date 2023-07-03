5a190ae89c953188ac813f61e36d248a62bd5493
Max Bazarov <bazaroffm@gmail.com>
2023-07-03 11:59:44 +0300

    add tests environent

---
+++ app/tests/unit_tests/test_auth/test_api.py
@@ -0,0 +1,31 @@
+from httpx import AsyncClient
+from pydantic import EmailStr
+import pytest
+
+
+@pytest.mark.parametrize('name,surname,email,password,status_code', [
+    ('Ivan', 'Ivanov', 'example@example.com', 'Password1!', 201),
+    ('Vasya', 'Vasyanov', 'example@example.com', 'Password2!', 400),
+    ('Bad', 'Password', 'test@test.com', 'bad_password', 422),
+])
+async def test_register_user(
+    name: str,
+    surname: str,
+    email: EmailStr,
+    password: str,
+    status_code: int,
+    async_client: AsyncClient
+):
+    response = await async_client.post(
+        '/auth/register',
+        json={
+            'name': name,
+            'surname': surname,
+            'email': email,
+            'password': password,
+        },
+    )
+
+    print(response)
+
+    assert response.status_code == status_code
