0792844a496951a3e20294aefd414b8223d32280
Vismut <pavel.epanovm@gmail.com>
2023-07-05 19:38:32 +0500

    Change **filter to **filter_by

--- app/core/service.py
+++ app/core/service.py
@@ -7,9 +7,9 @@ class BaseService:
     model: Base = None
 
     @classmethod
-    async def get_object_or_none(cls, **filter):
+    async def get_object_or_none(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model.__table__.columns).filter_by(**filter)
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().one_or_none()
 
