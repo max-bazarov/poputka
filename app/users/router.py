b21a6ee57855bc97cd49acb640628ed20b3fbf64
Vismut <pavel.epanovm@gmail.com>
2023-07-09 16:41:27 +0500

    Change BaseService to UserService in app/users/router

--- app/users/router.py
+++ app/users/router.py
@@ -4,21 +4,18 @@ from app.auth.dependencies import get_current_user
 
 from app.users.schemas import (Profile, UserBase)
 from app.users.models import User
-
-from app.core.service import BaseService
+from app.users.service import UserService
 
 router = APIRouter(prefix='/users', tags=['Users'])
 
 
 @router.get('/profile', status_code=status.HTTP_200_OK)
 async def get_current_user_private_info(user: User = Depends(get_current_user)) -> Profile:
-    BaseService.model = User
-    result = await BaseService.get_object_or_none(id=user.id)
+    result = await UserService.get_object_or_none(id=user.id)
     return result
 
 
 @router.get('/users/{user_uuid}', status_code=status.HTTP_200_OK)
 async def get_current_user_public_info(user_uuid: str) -> UserBase:
-    BaseService.model = User
-    result = await BaseService.get_object_or_none(id=user_uuid)
+    result = await UserService.get_object_or_none(id=user_uuid)
     return result
