3bf6829e92739d17fae20ec96ddffd82a5af57c6
Max Bazarov <bazaroffm@gmail.com>
2023-07-31 12:41:15 +0300

    refactor token logic for refresh endpoint

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 
 from fastapi import Depends, Request
+from fastapi.params import Cookie
 from jose import JWTError, jwt
 
 from app.auth.config import config
-from app.auth.exceptions import EmailTakenException, TokenAbsentException
+from app.auth.exceptions import (
+    EmailTakenException,
+    RefreshTokenNotValid,
+    TokenAbsentException,
+)
+from app.auth.models import RefreshToken
 from app.auth.schemas import UserAuthRegisterSchema
-from app.auth.service import UserService
+from app.auth.service import RefreshTokenService, UserService
+from app.users.models import User
 
 
 async def valid_user_create(
@@ -18,12 +25,34 @@ async def valid_user_create(
     return user
 
 
-async def valid_refresh_token():
-    pass
+async def valid_refresh_token(
+    refresh_token: str = Cookie(alias=config.REFRESH_TOKEN_KEY),
+) -> RefreshToken:
+    db_refresh_token = await RefreshTokenService.get_object_or_none(
+        refresh_token=refresh_token
+    )
+
+    if not db_refresh_token:
+        raise RefreshTokenNotValid()
+
+    if not await _is_valid_refresh_token(db_refresh_token):
+        raise RefreshTokenNotValid()
+
+    return db_refresh_token
 
 
-async def valid_refresh_token_user():
-    pass
+async def valid_refresh_token_user(
+    refresh_token: str = Depends(valid_refresh_token),
+) -> User:
+    user = await UserService.get_object_or_none(id=refresh_token.user_id)
+    if not user:
+        raise RefreshTokenNotValid()
+
+    return user
+
+
+async def _is_valid_refresh_token(refresh_token: RefreshToken):
+    return datetime.utcnow() <= refresh_token.expires_at
 
 
 async def get_token(request: Request):
