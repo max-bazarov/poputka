16fd81d60915c355a38a4c2a8de1f4279438c9ca
Max Bazarov <bazaroffm@gmail.com>
2023-07-31 12:42:57 +0300

    add default for pk into refresh token model

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from uuid import uuid4
 
 from sqlalchemy import UUID, Column, DateTime, ForeignKey, String
 
@@ -8,14 +9,12 @@ from app.database import Base
 class RefreshToken(Base):
     __tablename__ = 'refresh_token'
 
-    uuid = Column(UUID, primary_key=True)
+    uuid = Column(UUID, primary_key=True, default=uuid4)
     user_id = Column(ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     refresh_token = Column(String, nullable=False)
     expires_at = Column(DateTime, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(
         DateTime,
-        default=datetime.utcnow,
         onupdate=datetime.utcnow,
-        nullable=False,
     )
