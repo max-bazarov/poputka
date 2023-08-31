afd36aece33a8d3028817af68e1e89d7aa696ac9
Max Bazarov <bazaroffm@gmail.com>
2023-08-31 17:37:07 +0300

    update SA models to 2.0 style

--- app/users/models.py
+++ app/users/models.py
@@ -1,14 +1,12 @@
-from datetime import datetime
+from datetime import date, datetime
 
 from sqlalchemy import (
     TIMESTAMP,
-    Boolean,
-    Column,
     ForeignKey,
-    Integer,
     LargeBinary,
     String,
 )
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
 
@@ -16,18 +14,20 @@ from app.database import Base
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String(length=254), nullable=False)
-    name = Column(String(length=254), nullable=False)
-    surname = Column(String(length=254), nullable=False)
-    phone_number = Column(String)
-    car_id = Column(ForeignKey('car.id'))
-    age = Column(Integer)
-    likes = Column(Integer, default=0, nullable=False)
-    dislikes = Column(Integer, default=0, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(length=254), nullable=False)
+    name: Mapped[str] = mapped_column(String(length=254), nullable=False)
+    surname: Mapped[str] = mapped_column(String(length=254), nullable=False)
+    phone_number: Mapped[str] = mapped_column(String(length=12))
+    car_id: Mapped[int] = mapped_column(ForeignKey('car.id'))
+    age: Mapped[int]
+    likes: Mapped[int] = mapped_column(default=0, nullable=False)
+    dislikes: Mapped[int] = mapped_column(default=0, nullable=False)
 
-    registered_at = Column(TIMESTAMP, default=datetime.utcnow, nullable=False)
-    password = Column(LargeBinary, nullable=False)
-    is_admin = Column(Boolean, default=False, nullable=False)
-    is_active = Column(Boolean, default=False, nullable=False)
-    is_verified = Column(Boolean, default=False, nullable=False)
+    registered_at: Mapped[date] = mapped_column(
+        TIMESTAMP, default=datetime.utcnow, nullable=False
+    )
+    password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
+    is_admin: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
